7a2ca97281e1b016021898567142490b9f6e350a
SzJackiewicz <sz.jackiewicz@gmail.com>
2021-07-05 15:08:18 +0200

    body background

---
+++ my-app/src/README.md
@@ -0,0 +1,10 @@
+# GymDiary
+Track Your progress on gym!
+
+
+## troubleshooting
+
+### SET VERSION NODE 12.13
+```
+nvm use 12.13
+```
---
+++ my-app/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import styles from "..//Dashboard/Dashboard.module.scss"
+
+const Dashboard = () => {
+
+
+    
+    return (
+        <h1 className={styles.test}>DASHBOARD H1</h1>
+    )
+}
+
+export default Dashboard;
