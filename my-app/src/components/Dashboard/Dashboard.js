af48c5d6e6aa314b45108f5b61796ea92a324611
SzJackiewicz <sz.jackiewicz@gmail.com>
2021-07-05 16:01:48 +0200

    dashboard wrapper bgc

--- my-app/src/components/Dashboard/Dashboard.js
+++ my-app/src/components/Dashboard/Dashboard.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react'
 import styles from "..//Dashboard/Dashboard.module.scss"
 
 const Dashboard = () => {
-
-
-    
     return (
-        <h1 className={styles.test}>DASHBOARD H1</h1>
+        <div className={styles.wrapper}></div>
     )
 }
 
